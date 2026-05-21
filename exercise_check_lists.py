42f69c628b4acc4b34fe9fe739a07d2a3ae37c3d
LucaCorleto6 <lcorleto@mail.austral.edu.ar>
2026-05-21 15:36:51 -0300

    Update exercise_check_lists.py

--- exercise_check_lists.py
+++ exercise_check_lists.py
@@ -12,7 +12,7 @@ def check_lists(lista1, lista2):
     Returns:
         True si ambas listas tienen el mismo tercer elemento, False en caso contrario
     """
-        if len(lista1) >= 3 and len(lista2) >= 3:
+    if len(lista1) >= 3 and len(lista2) >= 3:
         return lista1[2] == lista2[2]
     else:
         return False
