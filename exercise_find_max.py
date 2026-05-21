2ba114fd38174aa3279051369bc866fcb0ccb260
LucaCorleto6 <lcorleto@mail.austral.edu.ar>
2026-05-21 15:36:42 -0300

    Update exercise_find_max.py

--- exercise_find_max.py
+++ exercise_find_max.py
@@ -11,7 +11,7 @@ def find_max(lista):
     Returns:
         El valor máximo de la lista o None si está vacía
     """
-        if len(lista) >= 1:
+    if len(lista) >= 1:
         return max(lista)
     else:
         return None
